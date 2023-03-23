03e4442ed43b66946bffa2a0a212a8060724aeb3
Fredrik <mittomrum@gmail.com>
2023-03-23 16:00:47 -0700

    Shoots away the ball at max height

--- Assets/PlatformController.cs
+++ Assets/PlatformController.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class PlatformController : MonoBehaviour
 {
+    private Rigidbody ballRigidbody;
     private bool isMoving = false;
-    [SerializeField] private float speed = 0f;
-    private float maxHeight = 0f;
     [SerializeField] private float accelarate = 0.1f;
+    private float speed = 0f;
+    private float maxHeight = 0f;
+    private float shootForce = 10f;
+
+    private void Start()
+    {
+        ballRigidbody = GameObject.FindWithTag("Ball").GetComponent<Rigidbody>();
+    }
 
     public void MovePlatformUp(float ballMass)
     {
@@ -28,6 +34,13 @@ public class PlatformController : MonoBehaviour
             Vector3 newPosition = transform.position + new Vector3(0, 1, 0) * speed;
             transform.position = newPosition;
             yield return null;
+        }
+
+        // Shoot the ball away when the platform reaches the maxHeight
+        if (isMoving)
+        {
+            Vector3 shootDirection = (ballRigidbody.transform.position - transform.position).normalized;
+            ballRigidbody.AddForce(shootDirection * shootForce, ForceMode.Impulse);
         }
 
         // Stop the platform when it reaches the maxHeight
